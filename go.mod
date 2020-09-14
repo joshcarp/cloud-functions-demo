c5c19d8945a36897b25df54c7337b71f0e0e1a76
joshcarp <joshuacarpeggiani@gmail.com>
2020-09-15 09:47:50 +1000

    Add gcloud bucket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/joshcarp/cloudfunctionsdemo
 
 go 1.14
+
+require github.com/joshcarp/gop v0.0.0-20200914071452-a78ba3fc78af // indirect
--- servehttp.go
+++ servehttp.go
@@ -1,7 +1,57 @@
 package cloudfunctionsdemo
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 
-func ServeHTTP(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello, world!"))
+	"cloud.google.com/go/storage"
+)
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	w.Write([]byte("Hello, " + name))
+	bucket := os.Getenv("bucketName")
+	latest, err := download(bucket, "latest")
+	if err != nil {
+		w.Write([]byte("An error occured"))
+	}
+	latestBytes, _ := ioutil.ReadAll(latest)
+	w.Write([]byte("Latest hello was to"))
+	w.Write(latestBytes)
+
+	upload(bucket, "latest", strings.NewReader(name))
+
+}
+
+func download(bucket, object string) (io.Reader, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+	return client.Bucket(bucket).Object(object).NewReader(ctx)
+}
+
+func upload(bucket string, object string, r io.Reader) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+	return nil
 }
