77c20d8d0bd9859556f203394ee5a6333235cd3c
joshcarp <joshuacarpeggiani@gmail.com>
2020-09-15 10:03:31 +1000

    Update to upload to gcs bucket

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/joshcarp/cloudfunctionsdemo
 
 go 1.14
 
-require github.com/joshcarp/gop v0.0.0-20200914071452-a78ba3fc78af // indirect
+require (
+	cloud.google.com/go/storage v1.10.0
+	github.com/joshcarp/gop v0.0.0-20200914071452-a78ba3fc78af // indirect
+)
--- servehttp.go
+++ servehttp.go
@@ -14,12 +14,12 @@ import (
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
-	w.Write([]byte("Hello, " + name))
-	bucket := os.Getenv("bucketName")
-	latest, _ := download(bucket, "latest")
-	if latest != nil {
+	w.Write([]byte("Hello, " + name + "\n"))
+	bucket := os.Getenv("bucket")
+	latest, err := download(bucket, "latest")
+	if err == nil {
 		latestBytes, _ := ioutil.ReadAll(latest)
-		w.Write([]byte("Latest hello was to\\"))
+		w.Write([]byte("Latest hello was to\n"))
 		w.Write(latestBytes)
 	} else {
 		w.Write([]byte("You're the first one!\n"))
